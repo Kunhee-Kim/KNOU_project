d7dab846637a305d1592d31e787e35adfbe30279
Kunhee-Kim <kghvic1@naver.com>
2023-12-10 16:35:15 -0500

    Print AUCs of first 2 models

--- debunbang_final.R
+++ debunbang_final.R
@@ -29,24 +29,44 @@ val_data <- data[inds$validation, ]
 
 
 # LR model 1
-model1 <- glm(target ~ gravity + ph + osmo + cond + urea + calc., data = train_data, family = binomial)
+model1 <- glm(target ~ gravity + ph + osmo + cond + urea + calc,
+              data = train_data, family = binomial)
 
 # backward selection 1
 model1_step <- step(model1, direction = "backward")
 summary(model1_step)
 vif(model1_step)
 
+# AUC
+par(mfrow = c(1,1))
+roc1_train <- roc(train_data$target, predict(model1_step, newdata = train_data, type="response"))
+plot(roc1_train)
+print(auc(roc1_train))
+roc1_val <- roc(val_data$target, predict(model1_step, newdata = val_data, type="response"))
+plot(roc1_val)
+print(auc(roc1_val))
 
 # LR model 2
-model2 <- glm(target ~ gravity * ph * osmo * cond * urea * calc, data = train_data, family = binomial)
+model2 <- glm(target ~ gravity * ph * osmo * cond * urea * calc,
+              data = train_data, family = binomial)
 
 # backward selection 2
 model2_step <- step(model2, direction = "backward")
 summary(model2_step)
 vif(model2_step)
 
+# AUC
+roc2_train <- roc(train_data$target, predict(model2_step, newdata = train_data, type="response"))
+plot(roc2_train)
+print(auc(roc2_train))
+roc2_val <- roc(val_data$target, predict(model2_step, newdata = val_data, type="response"))
+plot(roc2_val)
+print(auc(roc2_val))
+
+
 # LR model 1_1
-model1 <- glm(target ~ gravity + ph + cond + urea + calc, data = train_data, family = binomial)
+model1_1 <- glm(target ~ gravity + ph + cond + urea + calc,
+              data = train_data, family = binomial)
 
 # backward selection 1_1
 model1_1_step <- step(model1_1, direction = "backward")
@@ -68,7 +88,8 @@ plot(roc1_1_val)
 print(auc(roc1_1_val))
 
 # LR model 2_2
-model2_2 <- glm(target ~ gravity + calc + ph * cond * urea, data = train_data, family = binomial)
+model2_2 <- glm(target ~ gravity + calc + ph * cond * urea,
+                data = train_data, family = binomial)
 
 # backward selection 2_2
 model2_2_step <- step(model2_2, direction = "backward")
